9a8ef36cc4a8a1ae511506d23d1529436d768669
Albert Conceição <developer.albert@outlook.com>
2023-07-04 10:13:02 -0300

    feat: adding favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -49,7 +49,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-
+          <link rel="shortcut icon" href="./Logo.svg" />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
